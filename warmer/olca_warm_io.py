4cb22dfdfde94e2802729effebcacd090c48050c
a-w-beck <andrew.beck@erg.com>
2021-11-18 12:29:25 -0500

    extracted full baseline product system matrices

--- warmer/olca_warm_io.py
+++ warmer/olca_warm_io.py
@@ -23,7 +23,7 @@ def olca_tech_clean(df):
     Concatenate flows + categories (first 2 cols) into delimited strings;
     insert transposed copy of row labels as column headers to explicitly 
     assign implied identities of square matrix columns;
-    then melt to long format and split using delimiter.
+    then melt to long format, split using delimiter, and filter out 0's.
     """
     df['0'] = df['0'] + ';' + df['1'].fillna('NA')  # mitigates nan in 1st row
     df = df.drop('1', axis=1)
@@ -42,6 +42,9 @@ def olca_tech_clean(df):
     df = df[['from_flow','from_flow_unit','from_flow_cat',
             'to_flow','to_flow_unit','to_flow_cat','value']]
 
+    # # filter out 0 values (i.e., empty flows)
+    # df = df.query('value != 0')
+    
     return df
 
 def olca_intv_clean(df):
@@ -49,7 +52,7 @@ def olca_intv_clean(df):
     Concatenate midpoints + contexts (first 2 cols) into delimited strings;
     transpose and repeat concatenation w/ flows + categories;
     convert first row to column headers;
-    then melt to long format and split using delimiter.
+    then melt to long format, split using delimiter, and filter out 0's.
     """
     df['0'] = df['0'] + ';' + df['1']
     df = df.drop('1', axis=1).transpose()
@@ -60,16 +63,19 @@ def olca_intv_clean(df):
     # insert concat vals as column headers, then melt
     df.columns = df.iloc[0]
     df = df[1:]  # drop first row
-    df = pd.melt(df, id_vars='from_flow', var_name = 'to_flow')
+    df = pd.melt(df, id_vars='from_flow', var_name = 'to_flow')  
     
     # separate concatenated columns, plus extract units 
     post_melt_parse(df, 'from')
     post_melt_parse(df, 'to')
     
-    # # reorder columns
+    # reorder columns
     df = df[['from_flow','from_flow_unit','from_flow_cat',
             'to_flow','to_flow_unit','to_flow_cat','value']]
 
+    # filter out 0 values (i.e., empty flows)
+    df['value'] = pd.to_numeric(df['value'])
+    df = df.query('value != 0')
     
     return df
 
@@ -89,12 +95,21 @@ def post_melt_parse(df, fromto):
     return df
 
 if __name__ == '__main__':
-    base_tech = olca_mtx_read('technology_matrix.csv')
-    alt_tech = olca_mtx_read('technology_matrix_alt.csv')
-    base_intv = olca_mtx_read('intervention_matrix.csv')
-    alt_intv = olca_mtx_read('intervention_matrix_alt.csv')
+    tech_raw = olca_mtx_read('technology_matrix.csv')
+    intv_raw = olca_mtx_read('intervention_matrix.csv')
     
-    base_tech = olca_tech_clean(base_tech)
-    alt_tech = olca_tech_clean(alt_tech)
-    base_intv = olca_intv_clean(base_intv)
-    alt_intv = olca_intv_clean(alt_intv)
+    tech = olca_tech_clean(tech_raw)
+    intv = olca_intv_clean(intv_raw)
+    
+    # # comparing old & new (comprehensive) flow matrices
+    # tech_raw_old = olca_mtx_read('technology_matrix_old.csv')
+    # intv_raw_old = olca_mtx_read('intervention_matrix_old.csv')
+
+    # tech_old = olca_tech_clean(tech_raw_old)
+    # intv_old = olca_intv_clean(intv_raw_old)
+    
+    # temp_tech = tech.compare(tech_old)
+    # temp_intv = intv.compare(intv_old)
+    # temp_tech_raw = tech_raw.compare(tech_raw_old)
+    # temp_intv_raw = intv_raw.compare(intv_raw_old)
+    
