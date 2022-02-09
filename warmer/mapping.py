0d79f13a368c0c6de7aec9ee8d3488cadedd11fc
Ben Young <Ben.Young@erg.com>
2022-02-09 10:11:48 -0500

    add UUID as new field instead of replacing `to_flow_ID`

--- warmer/mapping.py
+++ warmer/mapping.py
@@ -8,13 +8,13 @@ from esupy.mapping import apply_flow_mapping
 
 def map_warmer_envflows(df):
 
-    # Align warmer fields with mapping
+    # Align warmer fields with mapping, value represents field in target df
     field_dict = {'SourceName': '',
                   'FlowableName':'to_flow_name',
                   'FlowableUnit':'to_flow_unit',
                   'FlowableContext':'to_flow_category',
                   'FlowableQuantity':'Amount',
-                  'UUID':'to_flow_ID'}
+                  'UUID':'FlowUUID'}
 
     df = apply_flow_mapping(df, 'WARM', flow_type='ELEMENTARY_FLOW',
                             keep_unmapped_rows=True, field_dict = field_dict,
