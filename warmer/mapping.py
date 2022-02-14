a1200d06d9f0d6d3258c7b08cb4944efebe2e8be
Ben Young <Ben.Young@erg.com>
2022-02-14 12:21:40 -0500

    add TargetProcessID to process mapping

--- warmer/mapping.py
+++ warmer/mapping.py
@@ -31,7 +31,7 @@ def map_useeio_processes(df):
     """Update process data based on process mapping file and apply conversions."""
     mapping = pd.read_csv(modulepath/'processmapping'/'processmapping.csv')
     mapping['ConversionFactor'] = mapping['ConversionFactor'].fillna(1)
-    mapping_cols = ['TargetProcessName', 'TargetUnit',
+    mapping_cols = ['TargetProcessName', 'TargetUnit', 'TargetProcessID',
                     'ConversionFactor', 'process ID']
     df = df.merge(mapping[mapping_cols],
                   how='left', left_on = ['from_process_ID'],
@@ -40,6 +40,7 @@ def map_useeio_processes(df):
                (df['TargetProcessName'].notnull())
     print(f'mapping {sum(criteria)} processes')
     df.loc[criteria, 'from_process_name'] = df['TargetProcessName']
+    df.loc[criteria, 'from_process_ID'] = df['TargetProcessID']
     df.loc[criteria, 'from_flow_unit'] = df['TargetUnit']
     df.loc[criteria, 'Amount'] = df['Amount'] * df['ConversionFactor']
     df.drop(columns=mapping_cols, inplace=True)
