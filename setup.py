6210fd8b1cf7561acb943477adca4202a85ae9d3
a-w-beck <andrew.beck@erg.com>
2022-08-17 17:13:16 -0400

    Initial packaging pass and updates to dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='WARMer',
     version='0.1.0',
     packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=['pandas>=1.1.0',
+    python_requires=">=3.8",
+    install_requires=['pandas>=1.4.0',
                       'numpy>=1.20.1',
                       ],
     extras_require={'olca': 'olca-ipc==0.0.10',
@@ -13,7 +13,7 @@ setup(
                         ['fedelemflowlist @ git+https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List.git@develop#egg=fedelemflowlist',
                          'esupy @ git+https://github.com/USEPA/esupy.git@develop#egg=esupy']},
     url='https://github.com/USEPA/WARMer.git',
-    author='Andrew Beck, Ben Young, and Wesley Ingwersen',
+    author='Andrew Beck, Ben Young, Jorge Vendries, and Wesley Ingwersen',
     author_email='ingwersen.wesley@epa.gov',
     description='Extract and process data from WARM openLCA database'
 )
